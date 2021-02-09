e05563633be1bbb80dab348e0903464185c88e4f
RafaDom10 <r-domingues@outlook.com>
2021-02-09 17:37:55 -0300

    mobile initial commit

---
+++ front-web/src/App.js
@@ -0,0 +1,20 @@
+import GlobalStyle from './styles/GlobalStyle';
+import { Container } from './styles';
+
+import Orders from  './components/Orders'
+
+import logo from './images/logo.svg';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <img src={logo} alt="ezOrders" />
+        <Orders />
+      </Container>
+    </>
+  );
+}
+
+export default App;
